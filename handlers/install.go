78f095057b4d5a39000fa49da89585b5e29fe014
Caleb Denio <cjdenio44@gmail.com>
2020-08-09 12:55:03 -0400

    Switch redirect status code

--- handlers/install.go
+++ handlers/install.go
@@ -8,5 +8,5 @@ import (
 
 // HandleInstall redirects the user to the Slack installation
 func HandleInstall(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, fmt.Sprintf("https://slack.com/oauth/v2/authorize?scope=im:history,chat:write&client_id=%s&redirect_uri=%s&team=TEHRV8VC", os.Getenv("SLACK_CLIENT_ID"), os.Getenv("HOST")+"/code"), 307)
+	http.Redirect(w, r, fmt.Sprintf("https://slack.com/oauth/v2/authorize?scope=im:history,chat:write&client_id=%s&redirect_uri=%s&team=TEHRV8VC", os.Getenv("SLACK_CLIENT_ID"), os.Getenv("HOST")+"/code"), 302)
 }
