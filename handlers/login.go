ccfe8760b7b25b181d6e6f5e37d4f5a5518facd5
Caleb Denio <cjdenio44@gmail.com>
2020-08-09 12:37:48 -0400

    Add new bot scope

--- handlers/login.go
+++ handlers/login.go
@@ -8,5 +8,5 @@ import (
 
 // HandleLogin redirects the user to the Slack login
 func HandleLogin(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, fmt.Sprintf("https://slack.com/oauth/v2/authorize?scope=im:history&user_scope=im:history,mpim:history,channels:history,groups:history,chat:write,users:read&client_id=%s&redirect_uri=%s", os.Getenv("SLACK_CLIENT_ID"), os.Getenv("HOST")+"/code"), 307)
+	http.Redirect(w, r, fmt.Sprintf("https://slack.com/oauth/v2/authorize?scope=im:history,chat:write&user_scope=im:history,mpim:history,channels:history,groups:history,chat:write,users:read&client_id=%s&redirect_uri=%s", os.Getenv("SLACK_CLIENT_ID"), os.Getenv("HOST")+"/code"), 307)
 }
